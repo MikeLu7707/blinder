ea5d96135994619e8e2b7fe8405139b98abe164f
Michael <maybeuknow77@gmail.com>
2022-10-19 14:30:29 +0200

    commit

--- peaky/settings.py
+++ peaky/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env=environ.Env()
+environ.Env.read_env()
 
 password = "gugunormanlam77#"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--uhcc-vmo!o!ico%(27%om67ztdzbjtua((-(13o9&e*u4lf0d'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
